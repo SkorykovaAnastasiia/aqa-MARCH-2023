7d7b2cd07b6791a39f3468eaeebaa52792d9a1c0
Anastasiia Skorykova <nastyaandramonova@gmail.com>
2023-04-22 19:34:13 +0300

    Home Assignment Cucumber

--- src/test/java/org/prog/mysteps/SqlSteps.java
+++ src/test/java/org/prog/mysteps/SqlSteps.java
@@ -3,6 +3,8 @@ package org.prog.mysteps;
 import io.cucumber.java.en.Given;
 import org.prog.ProductDto.GoodsDto;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,9 +14,15 @@ public class SqlSteps {
     public static List<GoodsDto> goods;
 
     @Given("Get products from DB")
-    public void getProductFromDB() throws SQLException, ClassNotFoundException {
+    public void getProductFromDB() throws SQLException, ClassNotFoundException, UnknownHostException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "password");
+        String dbHostName = null;
+        if (InetAddress.getLocalHost().getHostName().equals("DESKTOP-ELJTHIH")) {
+            dbHostName = "jdbc:mysql://localhost:3306/db";
+        } else {
+            dbHostName = "jdbc:mysql://mysql_db_1:3306/db";
+        }
+        Connection con = DriverManager.getConnection(dbHostName, "user", "password");
         Statement stmt = con.createStatement();
 
         ResultSet sqlResults = stmt.executeQuery("SELECT * FROM RozetkaGoods");
